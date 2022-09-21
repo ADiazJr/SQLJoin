59ef348566f24a3acd07d4df5d4aec449dbca4dd
unknown <arturodiazmay@gmail.com>
2022-09-21 11:03:11 -0500

    changed save destination

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -11,30 +11,30 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
-
+SELECT * FROM movies_library.movies LEFT JOIN movies_library.users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+SELECT * FROM movies_library.movies RIGHT JOIN movies_library.users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
+SELECT * FROM movies_library.movies INNER JOIN movies_library.users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
-
+SELECT lead_studio, count(lead_studio) AS times_favorited FROM movies_library.movies INNER JOIN movies_library.users on movie_id = favorite_movie_id GROUP BY lead_studio ORDER BY times_favorited desc LIMIT 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
-
+SELECT avg(rotten_tomatoes) FROM movies_library.movies INNER JOIN movies_library.users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
-
+SELECT film, count(film) AS appearances FROM movies_library.movies GROUP BY film ORDER BY appearances desc LIMIT 2;
